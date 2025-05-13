766a10f162b208b10700cb073cbb086087200c22
jorlowski89 <jakub.orlowski89@gmail.com>
2025-05-13 16:55:04 +0200

    szkielet widoków + sw + manifest

---
+++ script.js
@@ -0,0 +1,5 @@
+
+window.addEventListener('load', () => {
+  window.addEventListener('online', () => console.log('Online'));
+  window.addEventListener('offline', () => console.log('Offline'));
+});
---
+++ sw.js
@@ -0,0 +1,35 @@
+
+const CACHE_NAME = 'pwa-static-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/notes.html',
+  '/weather.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME && caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
